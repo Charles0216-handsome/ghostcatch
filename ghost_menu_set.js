831f99b1e015bd48e2d2ebebd1c9512e137d45dd
Charles0216-handsome <boss0912604516@wagor.tc.edu.tw>
2024-08-26 16:55:14 +0800

    Update ghost_menu_set.js

--- ghost_menu_set.js
+++ ghost_menu_set.js
@@ -1,6 +1,6 @@
 menu_title='昌昌家族--捉鬼特攻隊';
 menu_items = new Array(
-  "國語期末考複習,data/b01-1.js"
+  "國語期末考複習,data/b01-0.js"
 , "社會期末考複習,data/b01-2.js"
 , "國語第五課,data/b01-3.js"
 , "國語第五+六課,data/b01-4.js"
